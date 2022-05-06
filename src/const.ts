9f822e9bfbe8492343e209f0e604104f9e326b99
andrey.ponomarenko <andrey.ponomarenko@semrush.com>
2022-05-07 02:29:26 +0300

    fx txt

--- src/const.ts
+++ src/const.ts
@@ -1,20 +1,19 @@
-import path from 'path';
+import path from 'path'
 
-export const BLANK_SIZE_Y = 3508;
-export const BLANK_SIZE_X = 2480;
-export const BLANK_PADDING = 100;
+export const BLANK_SIZE_Y = 3508
+export const BLANK_SIZE_X = 2480
+export const BLANK_PADDING = 100
 
-export const RIGHT_EDGE = BLANK_SIZE_X - BLANK_PADDING;
-export const TOP_EDGE = BLANK_PADDING;
-export const LEFT_EDGE = BLANK_PADDING;
-export const BOTTOM_EDGE = BLANK_SIZE_Y - BLANK_PADDING;
+export const RIGHT_EDGE = BLANK_SIZE_X - BLANK_PADDING
+export const TOP_EDGE = BLANK_PADDING
+export const LEFT_EDGE = BLANK_PADDING
+export const BOTTOM_EDGE = BLANK_SIZE_Y - BLANK_PADDING
 
-export const BLANK_COLOR = "#f2f2f2";
-export const TEXT_COLOR = "#191b23";
+export const BLANK_COLOR = '#f2f2f2'
+export const TEXT_COLOR = '#191b23'
 
-export const FONT_TYPE_MEDIUM = "medium";
-export const FONT_TYPE_SEMIBOLD = "semibold";
-export const FONT_TYPE_BOLD = "bold";
+export const FONT_TYPE_MEDIUM = 'medium'
+export const FONT_TYPE_SEMIBOLD = 'semibold'
+export const FONT_TYPE_BOLD = 'bold'
 
-export const ROOT_DIR = path.resolve(__filename, '..');
-export const ASSETS_DIR = path.resolve(ROOT_DIR, 'assets');
+export const ASSETS_DIR = path.resolve(__dirname, 'assets')
